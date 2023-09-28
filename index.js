95e389f011d178e4d098bd8bd4fcb631dea8212b
bno4 <bnduchemin@gmail.com>
2023-09-28 16:15:50 +0200

    formulaire terminé

--- index.js
+++ index.js
@@ -1,1 +1,132 @@
-console.log("test");
+// Etape 1 => selection des inputs
+const inputs = document.querySelectorAll(
+  'input[type="text"], input[type="password"]'
+);
+const progressBar = document.getElementById("progress-bar");
+
+let pseudo, email, password, confirmPass;
+
+// Etape 4 => création du message d'erreur dynamique avec en param la classe (tag), le texte d'erreur, et la condition valid (true ou false)
+const errorDisplay = (tag, message, valid) => {
+  const container = document.querySelector("." + tag + "-container");
+  const span = document.querySelector("." + tag + "-container > span");
+
+  if (!valid) {
+    container.classList.add("error");
+    span.textContent = message;
+  } else {
+    container.classList.remove("error");
+    span.textContent = message;
+  }
+};
+
+// Etape 3 création des checker d'input avec parametre value = e.target.value
+const pseudoChecker = (value) => {
+  if (value.length > 0 && (value.length < 3 || value.length > 20)) {
+    errorDisplay(
+      "pseudo",
+      "le pseudo doit faire entre 3 et 20 caractères",
+      false
+    );
+    pseudo = null;
+  } else if (!value.match(/^[a-zA-Z0-9_.-]*$/)) {
+    errorDisplay(
+      "pseudo",
+      "Le pseudo ne doit pas contenir de caractères spéciaux",
+      false
+    );
+    pseudo = null;
+  } else {
+    errorDisplay("pseudo", "", true);
+    pseudo = value;
+  }
+};
+
+const emailChecker = (value) => {
+  if (!value.match(/^[\w_-]+@[\w-]+\.[a-z]{2,4}$/i)) {
+    errorDisplay("email", "L'adresse mail n'est pas valide");
+    email = null;
+  } else {
+    errorDisplay("email", "", true);
+    email = value;
+  }
+};
+
+const passwordChecker = (value) => {
+  progressBar.classList = "";
+  if (
+    !value.match(
+      /^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$/
+    )
+  ) {
+    errorDisplay(
+      "password",
+      "Au moins 8 caractères, dont une majuscule un chiffre et un caractère spécial"
+    );
+    progressBar.classList.add("progressRed");
+    password = null;
+  } else if (value.length < 12) {
+    progressBar.classList.add("progressBlue");
+    errorDisplay("password", "", true);
+    password = value;
+  } else {
+    progressBar.classList.add("progressGreen");
+    errorDisplay("password", "", true);
+    password = value;
+  }
+  if (confirmPass) confirmChecker(confirmPass);
+};
+
+const confirmChecker = (value) => {
+  if (value !== password) {
+    errorDisplay("confirm", "les mots de passe ne correspondent pas");
+    confirmPass = false;
+  } else {
+    errorDisplay("confirm", "", true);
+    confirmPass = true;
+  }
+};
+// Etape 2 => Création écoute des événements lors de la frappe
+inputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    switch (e.target.id) {
+      case "pseudo":
+        pseudoChecker(e.target.value);
+        break;
+      case "email":
+        emailChecker(e.target.value);
+        break;
+      case "password":
+        passwordChecker(e.target.value);
+        break;
+      case "confirm":
+        confirmChecker(e.target.value);
+        break;
+      default:
+        nul;
+    }
+  });
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (pseudo && email && password && confirmPass) {
+    const data = {
+      // en js pseudo : pseudo,
+      // email : email, etc.
+      pseudo,
+      email,
+      password,
+    };
+    console.log(data);
+    inputs.forEach((input) => (input.value = ""));
+    progressBar.classList = "";
+    pseudo = null;
+    email = null;
+    password = null;
+    confirmPass = null;
+    alert("Inscription validée !");
+  } else {
+    alert("veuillez compléter tous les champs");
+  }
+});
